79ee44fde66ee5399d4295c29984ab3e5520524c
Kuldip singh Jatav <kuldipsj@Kuldips-MacBook-Air.local>
2025-08-30 18:12:57 +0530

    Commitc 1

---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+
+    std::cout << "It okay"<<std::endl;
+    string str = "Hello, World!";
+    std::cout << str << std::endl;
+    return 0;
+}
